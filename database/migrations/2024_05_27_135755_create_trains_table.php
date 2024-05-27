d05dd222b2fac4ca034858d763a7e7a5477e5af2
Cigno05 <paolocalcagni.82@gmail.com>
2024-05-27 16:04:08 +0200

    ADD table to DB

---
+++ database/migrations/2024_05_27_135755_create_trains_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('train_code', 100);
+            $table->string('brand', 100);
+            $table->string('departure_station', 50);
+            $table->datetime('departure_time');
+            $table->string('arrival_station', 50);
+            $table->datetime('arrival_time');
+            $table->tinyInteger('number_of_carriages');
+            $table->boolean('in_time')->default(true);
+            $table->boolean('deleted')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trains');
+    }
+};
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,7 @@
 
 @section('pageTitle')
 
-Home
+Train - Home
 
 @endsection
 
